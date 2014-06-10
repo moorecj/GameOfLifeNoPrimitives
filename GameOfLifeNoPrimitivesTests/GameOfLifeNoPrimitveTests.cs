aee7964dfe2673250e87524303c9cb95e8814ef7
moorecj <moore.cyril@gmail.com>
2014-06-10 16:36:24 -0400

    Initial commit of the Game of Life with no primitives. First test passes.

---
+++ GameOfLifeNoPrimitivesTests/GameOfLifeNoPrimitveTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TheGameOfLifeNoPrimitivesKata;
+
+
+namespace GameOfLifeNoPrimitivesTests
+{
+    [TestFixture]
+    public class GameOfLifeNoPrimitveTests
+    {
+        [Test]
+        public void AGameWithNoCells_ResultsInAGameWithNoCellsAfterOneTick()
+        {
+
+            GameOfLifeNoPrimitives game = new GameOfLifeNoPrimitives();
+
+            Assert.That( game.GetGame(), Is.Null);
+        }
+    }
+}
---
+++ TheGameOfLifeNoPrimitivesKata/GameOfLifeNoPrimitives.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameOfLifeCells;
+
+namespace TheGameOfLifeNoPrimitivesKata
+{
+    public class GameOfLifeNoPrimitives
+    {
+        List<Cell> cells;
+        
+        public List<Cell> GetGame()
+        {
+            return (cells);
+        }
+
+        public void Tick()
+        {
+
+        }
+        
+    }
+    
+}
