6692380b83cdaaf3a9bba63ad8791ebf235049b3
moorecj <moore.cyril@gmail.com>
2014-06-12 12:17:53 -0400

    Added Extensions/ExtensionsMethods.cs

---
+++ TheGameOfLifeNoPrimitivesKata/Extensions/ExtensionsMethods.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGameOfLifeNoPrimitivesKata.Extensions
+{
+    public static class ExtensionMethods
+    {
+        // Deep clone
+        public static T DeepClone<T>(this T a)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, a);
+                stream.Position = 0;
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
